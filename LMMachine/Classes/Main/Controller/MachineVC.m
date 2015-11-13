faa6a89c54e7f4eeae13df346d6e4bd774e89385
Leo <leoios@sina.com>
2015-11-13 19:48:47 +0800

    1. 添加大头针

--- LMMachine/Classes/Main/Controller/MachineVC.m
+++ LMMachine/Classes/Main/Controller/MachineVC.m
@@ -27,9 +27,15 @@
     _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 64.0f, CGRectGetWidth(self.view.bounds), CGRectGetWidth(self.view.bounds) * 2 / 3)];
     _mapView.delegate = self;
     _mapView.showsUserLocation = YES;
+    _mapView.showsCompass= NO;
     _mapView.showsScale = NO;
     [_mapView setUserTrackingMode:MAUserTrackingModeFollow animated:YES];
     [_mapView setZoomLevel:16.1f animated:YES];
+    
+    MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc] init];
+    pointAnnotation.coordinate = CLLocationCoordinate2DMake(39.998246, 116.418153);
+    
+    [_mapView addAnnotation:pointAnnotation];
     
     [self.view addSubview:_mapView];
 }
@@ -70,7 +76,7 @@
     return nil;
 }
 
-#pragma mark -
+#pragma mark - MAMapView 代理
 
 - (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation {
     
@@ -79,6 +85,23 @@
         // 取出当前位置的坐标
         NSLog(@"latitude : %f, longitude: %f", userLocation.coordinate.latitude, userLocation.coordinate.longitude);
     }
+}
+
+- (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id <MAAnnotation>)annotation {
+    
+    if ([annotation isKindOfClass:[MAPointAnnotation class]]) {
+        
+        static NSString *pointReuseIndentifier = @"pointReuseIndentifier";
+        MAPinAnnotationView *annotationView = (MAPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:pointReuseIndentifier];
+        if (!annotationView) {
+            annotationView = [[MAPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:pointReuseIndentifier];
+        }
+        annotationView.canShowCallout= YES; // 设置气泡可以弹出，默认为 NO
+        annotationView.draggable = YES;
+        annotationView.pinColor = MAPinAnnotationColorPurple;
+        return annotationView;
+    }
+    return nil;
 }
 
 @end
